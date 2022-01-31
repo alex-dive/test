693ca3f5d446b76164265b5130851910c52532ef
shane <sam86102228@yahoo.com.tw>
2022-01-31 23:51:40 +0800

    add hello world line

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.end('hi')
+  // res.send('hi')
+  res.end('hello world')
 });
 
 module.exports = router;
